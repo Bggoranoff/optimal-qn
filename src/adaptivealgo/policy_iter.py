2adc7e7899e3ccdece6e2fdd887115b08cc478ac
Boris Goranov <b.g.goranov@student.tudelft.nl>
2024-05-09 17:12:38 +0200

    feat(mdp): simulate mdp optimal policy from json

--- src/adaptivealgo/policy_iter.py
+++ src/adaptivealgo/policy_iter.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import numpy as np
+import time
 
 from adaptivealgo import cli_main
 from adaptivealgo.agent import Agent
@@ -21,7 +23,7 @@ def build_argument_parser():
         "--links",
         dest="n_links",
         type=int,
-        default="2",
+        default="4",
         help="Number of required links in the network.",
     )
     parser.add_argument(
@@ -35,7 +37,7 @@ def build_argument_parser():
         "--actions",
         dest="actions",
         type=str,
-        default="0.3, 0.5",
+        default="0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5",
         help="Comma-separated list of protocol probabilities of success (e.g. 0.25, 0.5, 0.75).",
     )
     parser.add_argument(
@@ -57,7 +59,14 @@ def build_argument_parser():
         dest="tol",
         type=float,
         default="1e-20",
-        help="Tolerance at which the policy is considered stable."
+        help="Tolerance at which the policy is considered stable.",
+    )
+    parser.add_argument(
+        "--output",
+        dest="output_path",
+        type=str,
+        default="./policy.json",
+        help="Path to save the policy.",
     )
     return parser
 
@@ -102,7 +111,7 @@ def update_policy(env: Environment, agent: Agent, s_idx: int):
         best_action = a_idx if value > best_value else best_action
         best_value = value if value > best_value else best_value
 
-    new_policy = np.zeros(agent.n_actions)
+    new_policy = [0 for _ in range(agent.n_actions)]
     new_policy[best_action] = 1
     agent.set_policy(state, new_policy)
 
@@ -176,7 +185,35 @@ def print_policy(agent: Agent, env: Environment):
 
         print(f"State {state}: {agent.get_policy(state)}")
 
-def run(n_links: int, f_thresh: float, actions: str, alpha: float, gamma: float, tol: float):
+def save_policy_json(agent: Agent, env: Environment, filename: str):
+    """
+    Save the policy of the agent to a JSON file
+
+    :param Agent agent: The agent object
+    :param Environment env: The environment object
+    :param str filename: The name of the file
+    """
+
+    policy = {}
+    for state in env.states:
+        if env.is_terminal(state):
+            continue
+
+        policy[str(state)] = int(np.argmax(agent.get_policy(state)))
+    
+    program_state = {
+        "n_links": env.n_links,
+        "f_thresh": env.f_thresh,
+        "actions": env.actions,
+        "alpha": env.alpha,
+        "gamma": env.gamma,
+        "policy": policy,
+    }
+
+    with open(filename, "w") as f:
+        json.dump(program_state, f)
+
+def run(n_links: int, f_thresh: float, actions: str, alpha: float, gamma: float, tol: float, output_path: str):
     """
     Perform policy iteration given the command-line arguments
 
@@ -186,6 +223,8 @@ def run(n_links: int, f_thresh: float, actions: str, alpha: float, gamma: float,
     :param float alpha: Specifies relationship between probablity and fidelity
     :param float gamma: Exponential memory decay constant
     """
+
+    start_time = time.process_time()
 
     ps = [float(p) for p in actions.replace(" ", "").split(",")]
     env = Environment(n_links, ps, f_thresh, alpha, gamma)
@@ -199,8 +238,9 @@ def run(n_links: int, f_thresh: float, actions: str, alpha: float, gamma: float,
         eval_policy(env, agent, tol)
         policy_stable = improve_policy(env, agent)
 
-    print(f"Policy iteration converged after {i} steps")
-    print_policy(agent, env)
+    end_time = time.process_time()
+    print(f"Policy iteration converged after {i} steps for {end_time - start_time} seconds")
+    save_policy_json(agent, env, output_path)
 
 def main():
     cli_main(build_argument_parser, run)
---
+++ src/adaptivealgo/simulate_policy.py
@@ -0,0 +1,89 @@
+from argparse import ArgumentParser
+import json
+
+from adaptivealgo import cli_main
+from adaptivealgo.simulator import Simulator
+
+
+def build_argument_parser():
+    """
+    Define the command-line arguments for running policy iteration
+    """
+
+    parser = ArgumentParser(
+        description="""
+        simulate_policy: simulates the Markov decision process optimal policy
+        and outputs number of steps.
+        """
+    )
+
+    parser.add_argument(
+        "--process",
+        dest="input_path",
+        type=str,
+        default="./policy.json",
+        help="Path to the policy file.",
+    )
+    parser.add_argument(
+        "--iterations",
+        dest="iterations",
+        type=int,
+        default=1000,
+        help="Number of iterations to run the simulation.",
+    )
+    
+    return parser
+
+def import_json(input_path: str) -> dict:
+    """
+    Import the policy file
+
+    :param str input_path: The path to the policy file
+    :returns dict: The policy
+    """
+    
+    with open(input_path, "r") as file:
+        return json.load(file)
+
+def simulate_policy(policy: dict, n_links: int, actions: list[float], f_thresh: float, alpha: float, gamma: float, iters: int) -> int:
+    """
+    Simulate the policy
+
+    :param dict policy: The policy
+    :param list[float] ps: The generation protocol probabilities
+    :param float f_thresh: The minimum fidelity threshold
+    :param float alpha: Relationship between probability and fidelity F_i = 1 - alpha * p_i
+    :param float gamma: Memory decay parameter
+    :returns int: The number of steps
+    """
+
+    sim = Simulator(policy, n_links, f_thresh, actions, alpha, gamma)
+    result = 0
+
+    for _ in range(iters):
+        result += sim.run()
+    
+    return result // iters
+
+def run(input_path: str, iterations: int):
+    """
+    Run the policy iteration algorithm
+
+    :param str input_path: The path to the policy file
+    """
+    
+    with open(input_path, "r") as file:
+        args = json.load(file)
+    
+    n_links = args["n_links"]
+    ps = args["actions"]
+    f_thresh = args["f_thresh"]
+    alpha = args["alpha"]
+    gamma = args["gamma"]
+    policy = args["policy"]
+
+    steps = simulate_policy(policy, n_links, ps, f_thresh, alpha, gamma, iterations)
+    print(f"Number of steps: {steps}")
+
+def main():
+    cli_main(build_argument_parser, run)
