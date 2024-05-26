39cbc4f008c9bd89bb1bf41a1e8cf0ec844ef80e
Boris Goranov <b.g.goranov@student.tudelft.nl>
2024-05-26 18:43:32 +0200

    feat(db): scripts for more links

---
+++ delftblue/policy_iter_9links.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name="policy_iter_5links"
+#SBATCH --time=10:10:00
+#SBATCH --ntasks=1
+#SBATCH --partition=compute-p2
+#SBATCH --mem-per-cpu=80GB
+
+#SBATCH --cpus-per-task=1
+#SBATCH --account=education-eemcs-courses-cse3000
+#SBATCH --output=/home/%u/OptimalNearTerm/delftblue/logs/%x_%j.out
+#SBATCH --error=/home/%u/OptimalNearTerm/delftblue/logs/%x_%j.err
+
+module load miniconda3
+
+unset CONDA_SHLVL
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
+conda activate myenv
+
+srun poetry run policyIter --links 9 --thresh 0.4 --actions "0.1, 0.2, 0.3, 0.4, 0.5" --alpha 1.0 --gamma 0.1 --tol 1e-6
+
+conda deactivat
